004b24e788476d27d168f6a6a7b8c0f8ad92ef2f
Praveen <dmu.praveen@gmail.com>
2023-10-11 10:58:41 +0530

    new pid library

--- pid_lib.py
+++ pid_lib.py
@@ -1,24 +1,95 @@
-
+from typing import Tuple
 
 
 class PID:
-    def __init__(self,Kp,Ki,Kd,limits,derivative_filter):
+    '''
+    PID controller
+    '''
+    def __init__(self,Kp:float,Ki:float,Kd:float,limits:Tuple[float,float],derivative_filter_omega:float=float('inf'),derivative_on_measurement:bool=False):
+        ''''
+        limits - Limits on the output of the PID controller based on the limits of the inputs of the control systme  Tuple of floats (a,b) where a <= b
+        derivative_filter_omega = omega for low pass derivative filter on derivative make sure it is larger than half the average sampling time 
+        derivaitve_on_measurement = Enable derivative on measurement instead of error
+        '''
+        self._Kp = Kp
+        self._Ki = Ki
+        self._Kd = Kd
+
+        self._derivative_filter_omega = derivative_filter_omega
+        self._tau = 1/self._derivative_filter_omega
+        self._derivative_on_measurement = derivative_on_measurement
+        self._limits = limits
+        assert(self._limits[0] <= self._limits[1])
+
         self._set_point = 0.0
+
+        self._pre_i = 0.0
+        self._pre_d = 0.0
+        self._pre_e = 0.0
         self._pre_measurement = 0.0
-        self._pre_filtered_measurement = 0.0
-        self._filtered_measurement = 0.0
-        self._intergrated_measurement = 0.0
-        self._derivative_filter = derivative_filter
-    
-    def set_set_point(self,set_point):
+
+    def set_set_point(self,set_point:float):
         self._set_point = set_point
 
-    def get_val(self,measurement,time_delta):
-        
-        self._intergrated_measurement += (self._pre_measurement+measurement)/2*
-        self._filtered_measurement = measurement*self._derivative_filter + self._filtered_measurement*(self._derivative_filter)
+    @property
+    def parameteres(self):
+        return (self._Kp,self._Ki,self._Kd,self._derivative_filter_omega)
+
+    def set_parameteres(self,Kp:float,Ki:float,Kd:float):
+        self._Kp = Kp
+        self._Ki = Ki
+        self._Kd = Kd
+
+    def clamp(self,val):
+        '''
+        clamp the value between the limits
+        '''
+        if(val < self._limits[0]):
+            return self._limits[0]
+        if(val > self._limits[1]):
+            return self._limits[1]
+        return val
+
+    def get_val(self,measurement:float,time_delta:float):
+        '''
+        p[n] = K_p*e[n]
+        i[n] = (Ki*T/2)*(e[n]+e[n-1])+i[n-1]
+        d[n] = (2*Kd)/(2*tau+T)*(e[n]-e[n-1])+(2*tau-T)/(2*tau+T)*d[n-1]
+        '''
+        e = self._set_point-measurement #error
+        T = time_delta 
+        tau = self._tau if (self._tau*2 > T) else T/2
+
+        #Proportional 
+        p =  self._Kp*e
+
+        #Integral
+        i = (self._Ki*T)/2*(e+self._pre_e)+self._pre_i
+
+        #derivative with low pass
+        d = (2*self._Kd)/(2*tau+T)*(e - self._pre_e)+(2*tau-T)/(2*tau+T)*self._pre_d
+
+        if(self._derivative_on_measurement):
+            #derivative on measurement with low pass
+            d = (2*self._Kd)/(2*tau+T)* (-(measurement-self._pre_measurement))+(2*tau-T)/(2*tau+T)*self._pre_d
+
+        output = p+i+d
+
+        self._pre_i = i
+        self._pre_d = d 
+        self._pre_e = e
+        self._pre_measurement = measurement
+
+        return self.clamp(output)
+
+    def __call__(self,measurement:float,time_delta:float):
+        '''
+        Return the calculated control value
+        '''
+        return self.get_val(measurement,time_delta)
 
 
 
 
-        self._pre_measurement = self._filtered_measurement
+
+
---
+++ pid_test_my_lib.py
@@ -0,0 +1,60 @@
+from pid_lib import PID
+from twin_rotor import Twin_Rotor
+from time import sleep
+from data_buffers import Data_Buffers
+from data_plotter import Create_Gui
+from data_logger import CSV_Logger
+import numpy as np
+from math import radians
+
+MAX_SPEED = 600*6
+Kp = 0.342
+Kd = 0.222
+Ki = 0.132
+Limits = (-MAX_SPEED//2,MAX_SPEED//2)
+constant =  MAX_SPEED//2
+class Stable_Contoller:
+    def __init__(self):
+        self.twin_rotor = Twin_Rotor()
+        self.pid = PID(Kp,Ki,Kd,Limits,derivative_filter_omega=5)
+        self.angle = 0
+        self.beta = 0.5
+
+    def __del__(self):
+        self.twin_rotor.stop()
+
+    def set_set_point(self,point):
+        self.pid.set_set_point(point)
+
+    def run(self):
+        time_delta = self.twin_rotor.update_readings()
+        angle = self.twin_rotor.imu.simple_pitch_from_g_fusion
+        # self.angle = angle*(self.beta)+self.angle*(1-self.beta)
+        val = MAX_SPEED/2*self.pid(angle,time_delta)
+
+        print(val)
+        self.twin_rotor.motors.set_speed(constant+val,-constant+val)
+        return val
+    
+    
+
+
+
+def main():
+    FILE_PATH =  "logging_test/csv_data_logger_57.csv"
+    data_logger = CSV_Logger(FILE_PATH)
+    controller = Stable_Contoller()
+    controller.set_set_point(radians(-60))
+    data_buffers = Data_Buffers(1000)
+    gui_application = Create_Gui(data_buffers)
+    gui_application.add_time_graph(lambda x: x.encoder1.data,"encoder1")
+    gui_application.add_time_graph(lambda x: np.arctan2(x.acc_x.numpy_data,x.acc_z.numpy_data),"pitch")#type:ignore
+    gui_application.start()
+    while True:
+        data_buffers.update_buffers(controller.twin_rotor)
+        controller.run()
+        data_logger.log(controller.twin_rotor)
+        sleep(0.001)
+
+if __name__ == "__main__":
+    main()
